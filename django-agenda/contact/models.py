d3388f2435ac370bbd3c478d85220437f4309b58
Gustavo <clintgdm@gmail.com>
2024-06-25 07:03:07 -0300

    .

--- django-agenda/contact/models.py
+++ django-agenda/contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+
+
+#makemigrations -> executar quando criar model
 
 # Create your models here.
+
+class Contact( models.Model ):
+    name = models.CharField(max_length=50) #field com char de no maximo 50 chars
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=70)
+    email = models.EmailField(max_length=254)
+    #por padrao esses itens são obrigatorios
+    #para colocar como opcional adicionar 'blank = True' depois do maxLength
+    created_at = models.DateTimeField(default=timezone.now) #quando o campo for criado;
+    description = models.TextField()
