1e6a23e952cd5de6854b540582413032d2855b48
Gustavo <clintgdm@gmail.com>
2024-06-29 15:05:02 -0300

    .

--- django-agenda/contact/views/contact_views.py
+++ django-agenda/contact/views/contact_views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from contact.models import Contact
+from django.http import Http404
 
 # Create your views here.
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show = True).order_by('id')[0:10]
     context = {
         'contacts' : contacts
     }
+    #print(contacts.query)
     return render(request,'contact/index.html',context)
+
+def contact(request, id):
+    contact = Contact.objects.filter(pk = id).first()
+    if contact is None:
+        #retornar uma pagina 404 
+        raise Http404
+    
+    
+    context = { 
+        'contact' : contact
+    }
+    return render(request,'contact/contact.html',context)
